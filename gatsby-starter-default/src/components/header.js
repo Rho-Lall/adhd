c7a0fb85034399d4a51776753ed96d88270ab5ee
Rho <rho@assume-wisely.com>
2022-06-16 22:42:52 -0600

    Update header css.

--- gatsby-starter-default/src/components/header.js
+++ gatsby-starter-default/src/components/header.js
@@ -5,26 +5,29 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      margin: `0 auto`,
-      padding: `var(--space-4) var(--size-gutter)`,
       display: `flex`,
       flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 'auto',
+      //margin: `0 auto`,
+      // padding: `var(--space-4) var(--size-gutter)`,
       boxShadow: '1px 1px 15px 0px #ccc'
     }}
   >
     <div
-      style={{
-        borderRight: '1px solid grey',  
+      style={{  
         flexGrow: 1,
       }}
     >
       <Link
         to="/"
         style={{
-          fontSize: `1em`,  
+          fontSize: `1.5em`,  
           textDecoration: `none`,
           float: 'right',
-          padding: '1em'
+          paddingRight: '.5em'
+
         }}
       >
         Rho Lall DOCS
@@ -35,6 +38,8 @@ const Header = ({ siteTitle }) => (
     <div
       style={{
         flexGrow: 4,
+        borderRight: '1px solid grey',
+        borderLeft: '1px solid grey',
         padding: '1em'
       }}      
     >
@@ -42,10 +47,9 @@ const Header = ({ siteTitle }) => (
     </div>
     
     <div
-      style={{
-        borderLeft: '1px solid grey',     
+      style={{ 
         flexGrow: 1,
-        padding: '1em'
+        paddingLeft: '.5em'
       }} 
     >
       🔍  Search . . .
