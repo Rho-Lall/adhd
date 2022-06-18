2a9f818c29e5bf2ba643a9be27fa8782449162ba
Rho <rho@assume-wisely.com>
2022-06-18 12:18:51 -0600

    Draft mdx.slug page template with graphql content.

---
+++ gatsby-starter-default/src/pages/accomodations-for-adhd/{mdx.slug}.js
@@ -0,0 +1,41 @@
+import { graphql } from 'gatsby'
+import * as React from 'react'
+import Layout from '../../components/layout'
+import { MDXRenderer } from 'gatsby-plugin-mdx'
+
+const Article = ({ data }) => {
+    return(
+        <Layout>
+            {/* I need to add the page title. I also need the meta set up for each page. The layout should accept data.mdx.title as a parameter. */}
+            {/* Setup links.  Update Blog page to link to each post - PART 6 in the tut.*/}
+            <h1>{data.mdx.frontmatter.title}</h1>
+
+        
+            <MDXRenderer>
+                {data.mdx.body}
+            </MDXRenderer>
+        </Layout>
+
+    )
+
+}
+
+export const query = graphql`
+query content ($id: String){
+    mdx(id: {eq: $id}) {
+      frontmatter {
+        title
+        chapter
+        keywords
+        publish(formatString: "MMMM DD, YYYY")
+        update(formatString: "MMMM DD, YYYY")
+        short
+      }
+      slug
+      body
+    }
+  }
+`
+
+
+export default Article
