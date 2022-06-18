b2105b4d1b5bcb32690304b32421bd5a7484cefa
Rho <rho@assume-wisely.com>
2022-06-18 11:10:45 -0600

    Draft mdx.slug routing.

---
+++ gatsby-starter-default/src/pages/{mdx.slug}.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const Article = () => {
+    return(
+        <Layout>
+            <p>Articles will go here.</p>
+
+        </Layout>
+
+    )
+
+}
+
+export default Article
