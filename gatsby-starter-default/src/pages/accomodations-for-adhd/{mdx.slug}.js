39557c7a5387f2a697ea8c3fb2613dc0737e9353
Rho <rho@assume-wisely.com>
2022-06-19 08:37:17 -0600

    Draft Table of Contents for article template.

--- gatsby-starter-default/src/pages/accomodations-for-adhd/{mdx.slug}.js
+++ gatsby-starter-default/src/pages/accomodations-for-adhd/{mdx.slug}.js
@@ -8,6 +8,7 @@ const Article = ({ data }) => {
         <Layout>
             {/* I need to add the page title. I also need the meta set up for each page. The layout should accept data.mdx.title as a parameter. */}
             {/* Setup links.  Update Blog page to link to each post - PART 6 in the tut.*/}
+            {/* Set up H2  Table of Contents.*/}
             <h1>{data.mdx.frontmatter.title}</h1>
 
         
